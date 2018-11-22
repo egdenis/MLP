f8bdaadfead36b8a6fb864b813bd2eb1c4eacf8f
Etienne Denis <egdenis@gmail.com>
2018-11-22 18:06:56 -0500

    Added dataset

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -24,9 +24,15 @@ class MultilayerPerceptron():
 		self.weights_ = []
 		self.biases_ = []
 		for i in range(self.num_layers-1):
-			weights_init, biases_init = self._init_layer_coef(self.layer_sizes[i],self.layer_sizes[i+1])
+			weights_init, biases_init = self._init_layer_coef(self.layer_sizes[i]+1,self.layer_sizes[i+1])
 			self.weights_.append(weights_init)
 			self.biases_.append(biases_init)
+
+		for i in range(self.num_layers-2):
+			self.weights_[i] = np.append(self.weights_[i], np.array([[0] for layer in range(self.layer_sizes[i]+1)]),axis = 1)
+		
+		for i in range(self.num_layers-1):
+			print(self.weights_[i])
 		#self.weights_ = [np.array([[0.87226208, 0.63434208,-0.65554529],[-0.550579,-0.2502144,-0.36035178]]), np.array([[0.91193366],[0.63712029],[0.618395]])]
 		print(self.weights_)
 
@@ -41,6 +47,7 @@ class MultilayerPerceptron():
 			bound *= 4
 
 		weights = self.random_state.uniform(bound, -bound, (fan_in, fan_out))
+		
 		biases =  np.zeros(fan_out)#self.random_state.uniform(bound, -bound, fan_out)
 		return weights, biases
 			
@@ -55,24 +62,30 @@ class MultilayerPerceptron():
 		Returns:
 
 		"""
-		
+		#print(activations)
 		for t in range(1,self.num_layers):
+			activations[t-1][-1] = 1
 			for i in range(activations[t].shape[0]):
-				weighted_sum = np.dot(self.weights_[t-1][:,i],activations[t-1]) + self.biases_[t-1][i] #before was preactivation
+				weighted_sum = np.dot(self.weights_[t-1][:,i],activations[t-1]) #+ self.biases_[t-1][i] #before was preactivation
 				activations[t][i] = self.activation.function(weighted_sum)
+
+			
+		#activations[-1] = self.activation.function(np.dot(self.weights_[-2][:,i],activations[-2]))
 
 ##### FORGOT biases
 ###### ISSUE IS THAT THE PREACTIVATION HAS AN EXTRA LAYER
 	def _backward_pass(self, activations, deltas):#activation_derivs, deltas):
+		#print("deltas",deltas)
 		for t in range(self.num_layers-2, -1, -1):
-			for i in range(len(self.weights_[t])):											#Need to change for different loss functions
+			for i in range(len(self.weights_[t])):
+				#print(t,i,self.weights_[t][i], deltas[t+1])											#Need to change for different loss functions
 				deltas[t][i] = np.dot(self.weights_[t][i],deltas[t+1])*activations[t][i]*(1-activations[t][i])
 
 	def _backprop(self, activations, weight_grad, deltas, y): #return deltas for biases!!!!!!
 		#preactivations = [np.zeros_like(layer) for layer in activations]
 
 		self._forward_pass(activations)#, preactivations)
-
+		#print("activations", activations)
 		#Computing the derivative of activation function with the weighted sum of input
 		#neurons (preactivions variable) to avoid calculating it twice, once in doing backpass
 		#and once for gradient calc
@@ -83,7 +96,7 @@ class MultilayerPerceptron():
 		# initilize the loss of the last layer 
 		# NOTE THAT THE ACTIVATION FOR LAST LAYER MAY BE DIFFERENT
 		deltas[-1] = (y - activations[-1])*activations[-1]*(1-activations[-1])
-
+		#print(activations[-1])
 		self._backward_pass(activations, deltas)#activation_derivs, deltas)
 		for t in range(len(weight_grad)):
 			for i in range(len(weight_grad[t])):
@@ -97,18 +110,20 @@ class MultilayerPerceptron():
 			
 			for x, y in zip(X,ys):
 				count+=1
-
+				#print("x",x)
 				activations = [np.zeros(size) for size in  self.layer_sizes]
+				activations[0] = x
+				activations = [np.append(layer,1) for layer in activations[:-1]] + [np.zeros(self.layer_sizes[-1])]
 				deltas = deepcopy(activations)
 				weight_grad = [np.full(layer.shape, np.nan) for layer in self.weights_] 				
-				activations[0] = x
+				
 
 
 				self._backprop(activations,weight_grad, deltas, y) # 0 -> regularization param
 
 
 				self.weights_ = [weights + self.learning_rate * grad for weights, grad in zip(self.weights_, weight_grad)]
-				#self.biases_ = [bias + self.learning_rate * delta * bias for bias, delta in zip(self.biases_, deltas[1:])]
+				self.biases_ = [bias + self.learning_rate * delta * bias for bias, delta in zip(self.biases_, deltas[1:])]
 				loss = deltas[-1]
 
 				if (count%100 == 0):
@@ -162,10 +177,23 @@ class Sigmoid(BaseActivation):
 def main():
 	random_seed = RandomState(1234567890)
 
-	X = np.array([[1,1],[0,1],[1,0],[0,0]])
-	y = np.array([0,1,1,0])
+	mean1 = (1, 2)
+	mean1 = (-2, 3)
 
-	mlp = MultilayerPerceptron(layer_sizes = [2,3,2,4,4,1], activation = Sigmoid(), solver = "SGD",
+	cov1 = [[1, 0], [0, 1]]
+	cov2 = [[-1, 0], [3, 1]]
+
+	x1 = np.random.multivariate_normal(mean1, cov1, (100))
+	x2 = np.random.multivariate_normal(mean1, cov2, (100))
+
+	X = np.concatenate((x1,x2),axis=0)
+	print(x1.shape)
+
+
+	#X = np.array([[5,5],[-4,5],[5,-4],[-4,-4]])
+	y = np.append(np.full((100),1),np.zeros(100))
+
+	mlp = MultilayerPerceptron(layer_sizes = [2,3,9,1], activation = Sigmoid(), solver = "SGD",
 								learning_rate = 0.5, learning_rate_init = 0.5, max_iter = 10000,
 								shuffle = True, random_state = random_seed, tol = 0.01)
 	
